2f35ea88977137e2cc8b281ae11e389d900aa6f8
Victor Hugo <victor.vh.andrade@gmail.com>
2025-08-23 15:49:54 -0300

    Add files via upload

---
+++ Projeto-2.py
@@ -0,0 +1,18 @@
+nome = 'Victor Hugo' # String
+idade = 30 # Integer
+ec = 'solteiro' # String
+endereço = 'Rua Pedro Álvares Cabral, Centro, Nilópolis, RJ. ' # String
+número = 21980590294 # Integer
+email = 'victor.vh.andrade@gmail.com' # String
+hobby = 'Guitarrista' # String
+estudando_programação = True # Boolean
+
+
+print(type(nome))
+print(type(idade))
+print(type(ec))
+print(type(endereço))
+print(type(número))
+print(type(email))
+print(type(hobby))
+print(type(estudando_programação))
